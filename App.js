b7fa5d325703e6787691a98f451ad9ecc9a6e44a
Vinay <vinaya515@berkeley.edu>
2023-10-28 22:11:41 -0700

    Fixed App.js React Navigation errors

--- App.js
+++ App.js
@@ -3,20 +3,48 @@ import "react-native-get-random-values";
 import { CONVEX_URL } from "@env";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from 'react';
 import Events from "./Events";
 const convex = new ConvexReactClient(CONVEX_URL, {
   unsavedChangesWarning: false,
 });
 
+function EventScreen() {
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+            <ConvexProvider client={convex}>
+                <Events />
+            </ConvexProvider>
+        </View>
+    );
+}
+
+function TaskScreen() {
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+            <ConvexProvider client={convex}>
+                <Tasks />
+            </ConvexProvider>
+        </View>
+    );
+}
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <ConvexProvider client={convex}>
-        <Events />
-      </ConvexProvider>
-    </View>
+    <NavigationContainer>{
+        <Stack.Navigator>
+        <Stack.Screen name="Events" component={EventScreen} />
+        <Stack.Screen name="Tasks" component={TaskScreen} />
+        </Stack.Navigator>
+    }</NavigationContainer> 
   );
 }
 
