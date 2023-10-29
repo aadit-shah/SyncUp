076a4e00d5583ef7a34087f96cdfaab537543d5f
Vinay <vinaya515@berkeley.edu>
2023-10-29 02:05:28 -0700

    Added UI Elements. but not rendering

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { ConvexProvider, ConvexReactClient } from "convex/react";
 import "react-native-get-random-values";
 import { CONVEX_URL } from "@env";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
@@ -10,6 +10,11 @@ import Events from "./Events";
 const convex = new ConvexReactClient(CONVEX_URL, {
   unsavedChangesWarning: false,
 });
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import Constants from "expo-constants"
+import SignInScreen from "./components/SignInScreen";
+
+
 
 function EventScreen() {
     return (
@@ -35,17 +40,32 @@ function TaskScreen() {
     );
 }
 
+
+
 const Stack = createNativeStackNavigator();
 
+
 export default function App() {
-  return (
-    <NavigationContainer>{
-        <Stack.Navigator>
-        <Stack.Screen name="Events" component={EventScreen} />
-        <Stack.Screen name="Tasks" component={TaskScreen} />
-        </Stack.Navigator>
-    }</NavigationContainer> 
-  );
+    return (
+    <ClerkProvider publishableKey="pk_test_c2F2ZWQtZ29iYmxlci04NC5jbGVyay5hY2NvdW50cy5kZXYk">
+        <SafeAreaView>
+            <View style={styles.container}>
+                <SignedOut>
+                    <SignInScreen />
+                </SignedOut>            
+                <SignedIn>
+                    <NavigationContainer>{
+                    <Stack.Navigator>
+                    <Stack.Screen name="Events" component={EventScreen} />
+                    <Stack.Screen name="Tasks" component={TaskScreen} />
+                    </Stack.Navigator>
+                    }</NavigationContainer>
+                </SignedIn>
+            </View> 
+        </SafeAreaView>  
+        
+    </ClerkProvider>
+    );
 }
 
 const styles = StyleSheet.create({
