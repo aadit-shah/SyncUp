f91d9c1e9982eafe3316d1b3edba94dca00664c8
Vinay <vinaya515@berkeley.edu>
2023-10-28 21:39:27 -0700

    Made changes to events

--- Events.js
+++ Events.js
@@ -3,7 +3,7 @@ import { useQuery } from "convex/react";
 import { api } from "./convex/_generated/api";
 
 export default function Events() {
-  const events = useQuery(api.events.get);
+  const events = useQuery(api.events.myEvents);
   return (
     <View style={styles.container}>
       {events?.map(({ _id, Name }) => (
--- convex/events.js
+++ convex/events.js
@@ -1,8 +1,39 @@
 import { query } from "./_generated/server";
 
-export const get = query({
-  args: {},
-  handler: async (ctx) => {
-    return await ctx.db.query("events").collect();
-  },
+
+// Gets all events that user is not subscribed too
+// export const availableEvents = query({
+//   args: {},
+//   handler: async (ctx) => {
+//     const identity = await ctx.auth.getUserIdentity();
+//     // if (!identity) {
+//     //     throw new Error("Unauthenticated call to mutation");
+//     // }
+//     return await ctx.db.query("events").filter((q) => ).collect();
+//   },
+// });
+
+// List subscribed events for user
+export const myEvents = query({
+    args: {},
+    handler: async (ctx) => {
+      return await ctx.db.query("events").collect();
+    },
 });
+
+// Number of Attendees for each event*/
+export const getNumAttendees = query({
+    args: {},
+    handler: async (ctx) => {
+      const events  = await ctx.db.query("events").collect();
+      return events.map(events => ({"event": events.Name, "num_attendees" : events.Attendees.length}))
+    },
+});
+
+// Host User Create a New Event
+export const 
+// Hose User Modify an Existing Event
+
+// Host User Delete an Event
+
+// Other User Subscribe to An Event 
